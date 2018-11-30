f4fda7d7dd37cefcee2587ae0012a61cf73a3ea9
jmach83 <jmach83@gmail.com>
2018-12-01 00:49:18 +0100

    add counter parent(counters)

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,24 +2,31 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    value: this.props.value
   };
 
-  renderTags() {
-    if (this.state.tags.lenght === 0) return <p />;
-  }
+  handleIncrement = () => {
+    this.setState({ value: this.state.value + 1 });
+  };
 
   render() {
     return (
       <div>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        <span className={this.getBadgeClasses()}>{this.state.value}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 }
 
